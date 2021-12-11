bf6a6ecee1f344d3c7dfc13bcaac72471e62dfcf
Yuuto Chinen <e215733@ie.u-ryukyu.ac.jp>
2021-12-11 17:37:14 +0900

    Report5_step2

--- app/src/main/java/jp/ac/uryukyu/ie/e215733/Exception.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215733/Exception.java
@@ -1,15 +1,18 @@
 package jp.ac.uryukyu.ie.e215733;
 
 public class Exception {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws NullPointerException {
         printLength(null);
-        
-        }
-        
+    }
         static void printLength(String str) {
-            
+
+            try{
             int i = str.length();
             System.out.println(str + "は" + i + "文字");
+            }
+            catch(NullPointerException e){
+                System.out.println("エラー：NullPointerException");
+                System.out.println(e.getMessage());
+            }
         }
     }
